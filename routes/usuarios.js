93cd63754f7b27176699a4f7b7da03608a9a3c42
Erickowski <daniel_perez_03081995@hotmail.com>
2020-10-10 20:26:15 -0500

    Implementando controllers para crear usuarios

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,8 +1,9 @@
 // Rutas para crear usuarios
 const express = require("express");
 const router = express.Router();
+const usuariosController = require("../controllers/usuarioController");
 
 // Crea un usuario api/usuarios
-router.post("/", () => {});
+router.post("/", usuariosController.crearUsuario);
 
 module.exports = router;
