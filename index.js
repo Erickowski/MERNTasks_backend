f1ce510daeb58315e43f7c5b241a50ea9f07c06a
Erickowski <daniel_perez_03081995@hotmail.com>
2020-10-10 19:44:21 -0500

    Hola mundo desde el servidor

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Hola muno");
+const express = require("express");
+
+// Crear el servidor
+
+const app = express();
+
+// Puerto de la app
+const PORT = process.env.PORT || 4000;
+
+// Definir la página principal
+app.get("/", (req, res) => {
+  res.send("Hola mundo");
+});
+
+// Arrancar el servidor
+app.listen(PORT, () => {
+  console.log(`El servidor está funcionando en el puerto ${PORT}`);
+});
