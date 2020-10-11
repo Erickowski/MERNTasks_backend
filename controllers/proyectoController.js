1fb2028e978334cdb8fec4be236cb03c2c5fe5da
Erickowski <daniel_perez_03081995@hotmail.com>
2020-10-11 13:36:27 -0500

    Eliminar un proyecto por su id

--- controllers/proyectoController.js
+++ controllers/proyectoController.js
@@ -67,12 +67,37 @@ exports.actualizarProyecto = async (req, res) => {
     }
 
     // Actualizar
-    proyecto = await Proyecto.findByIdAndUpdate(
+    proyecto = await Proyecto.findOneAndUpdate(
       { _id: req.params.id },
       { $set: nuevoProyecto },
       { new: true }
     );
     res.json({ proyecto });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Error en el servidor.");
+  }
+};
+
+// Eliminar un proyecto
+exports.eliminarProyecto = async (req, res) => {
+  try {
+    // Revisar el Id
+    let proyecto = await Proyecto.findById(req.params.id);
+
+    // Si el proyecto existe o no
+    if (!proyecto) {
+      return res.status(404).json({ msg: "Proyecto no encontrado." });
+    }
+
+    // Verificar el creador del proyecto
+    if (proyecto.creador.toString() !== req.usuario.id) {
+      return res.status(401).json({ msg: "No autorizado." });
+    }
+
+    // Eliminar el proyecto
+    await Proyecto.findOneAndRemove({ _id: req.params.id });
+    res.json({ msg: "Proyecto eliminado" });
   } catch (error) {
     console.log(error);
     res.status(500).send("Error en el servidor.");
