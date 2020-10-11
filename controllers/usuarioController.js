c28276a7ae035aae7d813b4da80257515a81f4e3
Erickowski <daniel_perez_03081995@hotmail.com>
2020-10-11 10:23:57 -0500

    Mensajes de error y registro en respuesta

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,8 +1,15 @@
 const Usuario = require("../models/Usuario");
 
 exports.crearUsuario = async (req, res) => {
+  // Extraer email y password
+  const { email, password } = req.body;
   try {
-    let usuario;
+    // Revistar que el usuario registrado sea unico
+    let usuario = await Usuario.findOne({ email });
+
+    if (usuario) {
+      return res.status(400).json({ msg: "El usuario ya existe" });
+    }
 
     // Crea el nuevo usuario
     usuario = new Usuario(req.body);
@@ -11,7 +18,7 @@ exports.crearUsuario = async (req, res) => {
     await usuario.save();
 
     // Mensaje de confirmación
-    res.send("Usuario creado correctamente");
+    res.json({ msg: "Usuario creado correctamente" });
   } catch (error) {
     console.error(error);
     res.status(400).send("Hubo un error");
