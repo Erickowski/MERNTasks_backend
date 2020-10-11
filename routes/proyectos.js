a08e3d417b4ee91fb1ec5fed43d5a66cb0fe6c20
Erickowski <daniel_perez_03081995@hotmail.com>
2020-10-11 12:31:32 -0500

    Validacion e insertar quien creo dicho proyecto

--- routes/proyectos.js
+++ routes/proyectos.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const proyectoController = require("../controllers/proyectoController");
+const auth = require("../middleware/auth");
+const { check } = require("express-validator");
 
 // Crea proyectos - api/proyectos
-router.post("/", proyectoController.crearProyecto);
+router.post(
+  "/",
+  auth,
+  [check("nombre", "El nombre del proyecto es obligatorio.").not().isEmpty()],
+  proyectoController.crearProyecto
+);
+
+router.get("/", auth, proyectoController.crearProyecto);
 
 module.exports = router;
