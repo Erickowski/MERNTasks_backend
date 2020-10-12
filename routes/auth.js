44f8943898a4c2e192d7f008c14e27b69a7eb8a2
Erickowski <daniel_perez_03081995@hotmail.com>
2020-10-11 19:05:42 -0500

    Autenticacion de usuarios finalizado

--- routes/auth.js
+++ routes/auth.js
@@ -6,13 +6,7 @@ const authController = require("../controllers/authController");
 const auth = require("../middleware/auth");
 
 // Autenticar un usuario - api/auth
-router.post("/", [
-  check("email", "Agrega un email válido").isEmail(),
-  check("password", "El password debe ser minimo de 6 caracteres").isLength({
-    min: 6,
-  }),
-  authController.autenticarUsuario,
-]);
+router.post("/", [authController.autenticarUsuario]);
 
 // Obtiene el usuario autenticado
 router.get("/", auth, authController.usuarioAutenticado);
